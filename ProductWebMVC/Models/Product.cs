f23e5d4b473513d8f65d27fa1931be8fd8e02d6f
adhik <adhik@DINESH>
2023-11-02 13:46:42 -0400

    Edit

---
+++ ProductWebMVC/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace ProductWebMVC.Models
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string GetText()
+        {
+            return $"{Id} \t {Price} \t {Description}";
+        }
+
+    }
+}
--- ProductWebMVC/Program.cs
+++ ProductWebMVC/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductWebMVC.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ProductContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("ProductContext")));
 
 var app = builder.Build();
 
@@ -11,6 +18,12 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<ProductContext>();
+    context.Database.EnsureCreated();
 }
 
 app.UseHttpsRedirection();
